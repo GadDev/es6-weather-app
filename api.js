8ed0f8f6d79ceb98dcfc9b0d0a9d5c78801155c0
algadaix <alexandre@qured.com>
2020-11-15 16:11:25 +0000

    adding comment old api request version

--- api.js
+++ api.js
@@ -10,3 +10,25 @@ export class API {
 		}
 	}
 }
+// USE XMLHttpRequest
+// export class API {
+// 	static fetchData(url) {
+// 		return new Promise((resolve, reject) => {
+// 			const HTTP = new XMLHttpRequest();
+// 			HTTP.open('GET', url);
+// 			HTTP.onreadystatechange = function () {
+// 				if (
+// 					HTTP.readyState == XMLHttpRequest.DONE &&
+// 					HTTP.status == 200
+// 				) {
+// 					const RES = JSON.parse(HTTP.responseText);
+// 					resolve(RES);
+// 				} else if (HTTP.readyState == XMLHttpRequest.DONE) {
+// 					reject('something went wrong');
+// 				}
+// 			};
+
+// 			HTTP.send();
+// 		});
+// 	}
+// }
